70559118f3754a3d0bc1b753daf1e72208726b22
ryan <kalansiryan@gmail.com>
2025-04-26 06:56:33 +0700

    update map

--- src/scripts/utils/map.js
+++ src/scripts/utils/map.js
@@ -1,4 +1,15 @@
 import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const MapUtils = {
   initMap({ container, center = [0, 0], zoom = 2, onClickCallback = null }) {
