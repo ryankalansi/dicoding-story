ab443d1351a4c8a1eb4adc278d55c4ab558ee5e8
ryan <kalansiryan@gmail.com>
2025-04-26 06:48:05 +0700

    change base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dicoding-story/",
+  base: "/",
   root: resolve(__dirname, "src"),
   publicDir: resolve(__dirname, "src", "public"),
   build: {
